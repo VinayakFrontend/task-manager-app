3d3f2233b3c781d99006b6ac14fb8f4af1993c64
Vinayak Gupta <guptavinayak1614@gmail.com>
2025-07-11 12:39:02 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,19 +1,10 @@
-// import axios from 'axios';
-// const API = axios.create({ baseURL: 'http://localhost:5000/api' });
 
-// API.interceptors.request.use((req) => {
-//   const token = localStorage.getItem('token');
-//   if (token) req.headers['x-auth-token'] = token;
-//   return req;
-// });
-
-// export default API;
 
 
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://task-manager-app-xp9b.onrender.com/api',
 });
 
 // Attach JWT token
